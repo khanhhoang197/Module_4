954c69535fa97a2bdd51c6652413a4762083fa4e
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-11-01 09:28:14 +0700

    upload file

---
+++ UploadFile/src/main/java/com/khanhhoang/uploadfile/service/IProductService.java
@@ -0,0 +1,18 @@
+package com.khanhhoang.uploadfile.service;
+
+import com.khanhhoang.uploadfile.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+}
