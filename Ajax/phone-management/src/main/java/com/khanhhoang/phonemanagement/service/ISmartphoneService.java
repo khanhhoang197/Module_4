a31d853a5f2b60eb804fb67b6d05c396a54d1b05
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-11-08 09:18:35 +0700

    smart phone

---
+++ Ajax/phone-management/src/main/java/com/khanhhoang/phonemanagement/service/ISmartphoneService.java
@@ -0,0 +1,15 @@
+package com.khanhhoang.phonemanagement.service;
+
+import com.khanhhoang.phonemanagement.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
